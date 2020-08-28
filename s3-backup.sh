1ae223ac4b12ad08d620aa3b2cc03a3317b76fb2
Quentin Bramas <bramas@unistra.fr>
2020-08-28 09:20:43 +0000

    fix s3 backup script so that it can run in crontab

--- s3-backup.sh
+++ s3-backup.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")";
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE=backup_$DATE.sql
@@ -7,6 +9,6 @@ BACKUP_FILE=backup_$DATE.sql
 docker-compose exec -T mysql mysqldump -uroot -proot hotcrp > $BACKUP_FILE
 
 #upload it to S3
-docker run --rm -it -v aws:/root/.aws -v $(pwd):/aws amazon/aws-cli s3 cp $BACKUP_FILE s3://opodis-backup
+docker run --rm -i -v aws:/root/.aws -v $(pwd):/aws amazon/aws-cli s3 cp $BACKUP_FILE s3://opodis-backup
 
 rm $BACKUP_FILE
